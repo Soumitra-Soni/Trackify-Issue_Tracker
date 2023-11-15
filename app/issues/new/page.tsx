499c919bb4a465e7d88552f692fff062405fd108
Soumitra-Soni <somitrasoni123@gmail.com>
2023-11-15 19:28:49 +0530

    Error Handle

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
-import { TextField, Button } from "@radix-ui/themes";
+import { TextField, Button, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
+import { FaExclamationTriangle } from "react-icons/fa";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
@@ -12,49 +13,46 @@ interface IssueForm {
   description: string;
 }
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
-
 const NewIssuePage = () => {
-<<<<<<< HEAD
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-  return (
-    <form
-      className="max-w-xl space-y-10"
-      onSubmit={handleSubmit((data) => console.log(data))}
-=======
   const router = useRouter();
+  const [error, setError] = React.useState<string | null>(null);
   const { register, control, handleSubmit } = useForm<IssueForm>();
-  console.log(register);
   return (
-    <form
-      className="max-w-xl space-y-10"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
->>>>>>> 4749fc8f94a47414d41e23f9d5b175fe23a71a78
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-<<<<<<< HEAD
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-=======
-        render={(feild) => <SimpleMDE placeholder="Description" {...feild} />}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-10">
+          <Callout.Icon className="text-red-500">
+            <FaExclamationTriangle />
+          </Callout.Icon>
+          <Callout.Text className="text-red-500">{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
->>>>>>> 4749fc8f94a47414d41e23f9d5b175fe23a71a78
-      <Button>Submit New Issue</Button>
-    </form>
+      <form
+        className="space-y-10"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An Unexpected Error Occurred");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
