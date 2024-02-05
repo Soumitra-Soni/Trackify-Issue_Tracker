834ab8901ed1ec5bc8ec7e3ef0c9af6faac469f3
Soumitra-Soni <somitrasoni123@gmail.com>
2024-02-05 16:02:49 +0530

    Assignee Select Component.

---
+++ app/issues/[id]/Edit/AssigneeSelect.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { User } from "next-auth";
+import React, { useEffect, useState } from "react";
+
+const AssigneeSelect = () => {
+  const [user, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  });
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {user.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+          {/* <Select.Item value="1">Soumitra Soni</Select.Item> */}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default AssigneeSelect;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import IssueDetails from "./Edit/IssueDetails";
 import DeleteIssueButton from "./Edit/DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import AssigneeSelect from "./Edit/AssigneeSelect";
 
 interface Props {
   params: { id: string };
@@ -33,6 +34,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="4">
+            <AssigneeSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
